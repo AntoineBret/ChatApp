7655ece378bb8968e8e12e6aa76383f36ac5b57e
Antoine <Antoine.bret@live.fr>
2018-06-25 16:55:55 +0200

    json @Post succes

--- app/src/main/java/com/chatapp/ipme/chatapp/remote/ApiEndPointInterface.java
+++ app/src/main/java/com/chatapp/ipme/chatapp/remote/ApiEndPointInterface.java
@@ -4,55 +4,55 @@ import com.chatapp.ipme.chatapp.model.Login;
 import com.chatapp.ipme.chatapp.model.Message;
 import com.chatapp.ipme.chatapp.model.Room;
 import com.chatapp.ipme.chatapp.model.User;
+import com.google.gson.annotations.JsonAdapter;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import io.reactivex.Observable;
+import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiEndPointInterface {
 
-  @FormUrlEncoded
+    //User
+    @POST("/login")
+    @Headers({ "Content-Type: application/json;charset=UTF-8"})
+    Observable<Login> loginUser(@Body HashMap<String, String> map);
 
-  //Users
+    @GET("/users")
+    Observable<List<User>> getUsers();
+    //todo
 
-  @POST("/users/register")
-  Observable<Login> createUser(@Field("username") String username,
-                               @Field("password") String password);
+    //Messages
 
-  @GET("/users")
-  Observable<List<User>> getUsers();
-  //todo
+    @POST("/messages")
+    Observable<Message> createMessage(@Field("id") Integer id,
+                                      @Field("content") String content,
+                                      @Field("createdAt") Date createdAt);
 
-  //Messages
+    @GET("messages")
+    Observable<Message> getMessage(@Query("id") Integer id,
+                                   @Query("content") String content,
+                                   @Query("createdAt") Date createdAt);
+    //Room
 
-  @POST("/messages")
-  Observable<Message> createMessage(@Field("id") Integer id,
-                                    @Field("content") String content,
-                                    @Field("createdAt") Date createdAt);
+    @POST("/rooms")
+    Observable<Room> createRoom(@Field("name") String name,
+                                @Field("users") List users,
+                                @Field("messages") List messages);
 
-  @GET("messages")
-  Observable<Message> getMessage(@Query("id") Integer id,
-                                 @Query("content") String content,
-                                 @Query("createdAt") Date createdAt);
-  //Room
-
-  @POST("/rooms")
-  Observable<Room> createRoom(@Field("name") String name,
-                              @Field("users") List users,
-                              @Field("messages") List messages);
-
-  @GET("users")
-  Observable<Room> getRoom(@Query("id") Integer id,
-                           @Query("name") String name,
-                           @Query("sizeMax") Integer sizeMax,
-                           @Query("language") String language,
-                           @Query("users") List users,
-                           @Query("messages") List messages);
+    @GET("users")
+    Observable<Room> getRoom(@Query("id") Integer id,
+                             @Query("name") String name,
+                             @Query("sizeMax") Integer sizeMax,
+                             @Query("language") String language,
+                             @Query("users") List users,
+                             @Query("messages") List messages);
 }
--- app/src/main/java/com/chatapp/ipme/chatapp/ui/login/LogInFragment.java
+++ app/src/main/java/com/chatapp/ipme/chatapp/ui/login/LogInFragment.java
@@ -17,11 +17,15 @@ import android.widget.TextView;
 
 import com.chatapp.ipme.chatapp.R;
 import com.chatapp.ipme.chatapp.model.Login;
+import com.chatapp.ipme.chatapp.model.User;
 import com.chatapp.ipme.chatapp.remote.ApiClient;
 import com.chatapp.ipme.chatapp.remote.ApiEndPointInterface;
 import com.chatapp.ipme.chatapp.ui.room.RoomFragment;
 import com.chatapp.ipme.chatapp.utils.AlertDialogManager;
 import com.chatapp.ipme.chatapp.utils.SessionManager;
+
+import java.net.ConnectException;
+import java.util.HashMap;
 
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -34,169 +38,176 @@ import static com.chatapp.ipme.chatapp.utils.AnimationManager.frameTransition;
 
 public class LogInFragment extends Fragment {
 
-  public static LogInFragment newInstance() {
-    return new LogInFragment();
-  }
+    public static LogInFragment newInstance() {
+        return new LogInFragment();
+    }
 
-  private AlertDialogManager alert = new AlertDialogManager();
-  private SessionManager session;
-  private FrameLayout frameLayout;
-  private ViewModel viewModel;
-  private View rootView;
-  private AnimatorSet flip;
+    private AlertDialogManager alert = new AlertDialogManager();
+    private SessionManager session;
+    private FrameLayout frameLayout;
+    private ViewModel viewModel;
+    private View rootView;
+    private AnimatorSet flip;
 
-  private String signInLog;
-  private String signInPassword;
-  private String logInLog;
-  private String logInPassword;
+    private EditText inputLog;
+    private EditText inputPassword;
+    private String signInLog;
+    private String signInPassword;
+    private String logInLog;
+    private String logInPassword;
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    viewModel = ViewModelProviders.of(this).get(LogInViewModel.class);
-    session = new SessionManager(getContext());
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewModel = ViewModelProviders.of(this).get(LogInViewModel.class);
+        session = new SessionManager(getContext());
 
-    getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-      WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-    flip = (AnimatorSet) AnimatorInflater.loadAnimator(getContext(), R.animator.flip);
-  }
+        flip = (AnimatorSet) AnimatorInflater.loadAnimator(getContext(), R.animator.flip);
+    }
 
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    rootView = inflater.inflate(R.layout.fragment_login, container, false);
-    frameLayout = rootView.findViewById(R.id.login_frame_container);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        frameLayout = rootView.findViewById(R.id.login_frame_container);
 
-    frameTransition(frameLayout, flip);
-    LayoutInflater.from(getContext()).inflate(R.layout.welcome_cardview, frameLayout, true);
-    
-    firstConnectionFrame();
+        frameTransition(frameLayout, flip);
+        LayoutInflater.from(getContext()).inflate(R.layout.welcome_cardview, frameLayout, true);
 
-    return rootView;
-  }
+        firstConnectionFrame();
 
-  //First connection Accept Terms and Conditions
-  private void firstConnectionFrame() {
-    Button buttonTermServiceAgree = rootView.findViewById(R.id.buttonTermServiceAgree);
-    buttonTermServiceAgree.setOnClickListener(view -> {
-      loginUser();
-    });
-  }
+        return rootView;
+    }
 
-  //Create account
-  private void signInUser() {
-    frameTransition(frameLayout, flip);
-    LayoutInflater.from(getContext()).inflate(R.layout.signin_cardview, frameLayout, true);
+    //First connection Accept Terms and Conditions
+    private void firstConnectionFrame() {
+        Button buttonTermServiceAgree = rootView.findViewById(R.id.buttonTermServiceAgree);
+        buttonTermServiceAgree.setOnClickListener(view -> {
+            loginUser();
+        });
+    }
 
-    Button buttonCreate = rootView.findViewById(R.id.buttonCreate);
-    TextView tvAlreadyAccount = rootView.findViewById(R.id.tvAlreadyAccount);
-    EditText inputUsernameCreate = rootView.findViewById(R.id.inputUsernameCreate);
-    EditText inputPasswordCreate = rootView.findViewById(R.id.inputPasswordCreate);
+    //Create account
+    private void signInUser() {
+        frameTransition(frameLayout, flip);
+        LayoutInflater.from(getContext()).inflate(R.layout.signin_cardview, frameLayout, true);
 
-    signInLog = inputUsernameCreate.getText().toString();
-    signInPassword = inputPasswordCreate.getText().toString();
+        Button buttonCreate = rootView.findViewById(R.id.buttonCreate);
+        TextView tvAlreadyAccount = rootView.findViewById(R.id.tvAlreadyAccount);
+        EditText inputUsernameCreate = rootView.findViewById(R.id.inputUsernameCreate);
+        EditText inputPasswordCreate = rootView.findViewById(R.id.inputPasswordCreate);
 
-    buttonCreate.setOnClickListener(view -> {
-      createAccount();
-    });
+        signInLog = inputUsernameCreate.getText().toString();
+        signInPassword = inputPasswordCreate.getText().toString();
 
-    tvAlreadyAccount.setOnClickListener(v -> {
-      loginUser();
-    });
-  }
+        buttonCreate.setOnClickListener(view -> {
+           // createAccount();
+        });
 
-  //Connect with existing account
-  private void loginUser() {
-    frameTransition(frameLayout, flip);
-    LayoutInflater.from(getContext()).inflate(R.layout.login_cardview, frameLayout, true);
+        tvAlreadyAccount.setOnClickListener(v -> {
+            loginUser();
+        });
+    }
 
-    Button buttonLogIn = rootView.findViewById(R.id.buttonLog);
-    TextView tvNoAccount = rootView.findViewById(R.id.tvNoAccount);
-    EditText inputLog = rootView.findViewById(R.id.inputLog);
-    EditText inputPassword = rootView.findViewById(R.id.inputPassword);
+    //Connect with existing account
+    private void loginUser() {
+        frameTransition(frameLayout, flip);
+        LayoutInflater.from(getContext()).inflate(R.layout.login_cardview, frameLayout, true);
 
-    logInLog = inputLog.getText().toString();
-    logInPassword = inputPassword.getText().toString();
+        Button buttonLogIn = rootView.findViewById(R.id.buttonLog);
+        TextView tvNoAccount = rootView.findViewById(R.id.tvNoAccount);
+        inputLog = rootView.findViewById(R.id.inputLog);
+        inputPassword = rootView.findViewById(R.id.inputPassword);
 
-    buttonLogIn.setOnClickListener(view -> {
-      connectAccount();
-    });
+        buttonLogIn.setOnClickListener(view -> {
+            connectAccount();
+        });
 
-    tvNoAccount.setOnClickListener(v -> {
-      signInUser();
-    });
-  }
+        tvNoAccount.setOnClickListener(v -> {
+            signInUser();
+        });
+    }
 
-  private void connectAccount() {
-    ApiEndPointInterface apiInterface = ApiClient.getClient().create(ApiEndPointInterface.class);
-    apiInterface.createUser(logInLog, logInPassword)
-      .subscribeOn(Schedulers.io())
-      .observeOn(AndroidSchedulers.mainThread())
-      .subscribe(new Observer<Login>() {
-        @Override
-        public void onSubscribe(Disposable d) {
-        }
+    private void connectAccount() {
+        logInLog = inputLog.getText().toString();
+        logInPassword = inputPassword.getText().toString();
+        ApiEndPointInterface apiInterface = ApiClient.getClient().create(ApiEndPointInterface.class);
+        HashMap<String,String> map  = new HashMap<>();
+        map.put("username",logInLog);
+        map.put("password", logInPassword);
+        apiInterface.loginUser(map)
 
-        @Override
-        public void onNext(Login user) {
-          if (logInLog.trim().length() > 0 && logInPassword.trim().length() > 0) {
-            if (logInLog.equals(logInLog) && logInPassword.equals(logInPassword)) {
-              session.createLoginSession(logInLog, logInPassword);
-              Fragment f = RoomFragment.newInstance();
-              getFragmentManager().beginTransaction().replace(R.id.frame_container, f).addToBackStack(null).commit();
-            } else {
-              alert.showAlertDialog(getContext(), "Login failed..", "Username/Password is incorrect", false);
-            }
-          } else {
-            alert.showAlertDialog(getContext(), "Login failed..", "Please enter username and password", false);
-          }
-        }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Login>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
 
-        @Override
-        public void onError(Throwable t) {
-          alert.showAlertDialog(getContext(), "java.net.ConnectException:", "Failed to connect to" + BASE_URL, false);
-        }
+                    @Override
+                    public void onNext(Login user) {
+                        if (logInLog.trim().length() > 0 && logInPassword.trim().length() > 0) {
+                            if (logInLog.equals(logInLog) && logInPassword.equals(logInPassword)) {
+                                session.createLoginSession(logInLog, logInPassword);
+                                Fragment f = RoomFragment.newInstance();
+                                getFragmentManager().beginTransaction().replace(R.id.frame_container, f).addToBackStack(null).commit();
+                            } else {
+                                alert.showAlertDialog(getContext(), "Login failed..", "Username/Password is incorrect", false);
+                            }
+                        } else {
+                            alert.showAlertDialog(getContext(), "Login failed..", "Please enter username and password", false);
+                        }
+                    }
 
-        @Override
-        public void onComplete() {
-        }
-      });
-  }
+                    @Override
+                    public void onError(Throwable t) {
+                        if (t instanceof ConnectException) {
+                            alert.showAlertDialog(getContext(), "java.net.ConnectException:", "Failed to connect to" + BASE_URL, false);
+                        }
+                    }
 
-  private void createAccount() {
-    //todo : improve logic duplicate
-    ApiEndPointInterface apiInterface = ApiClient.getClient().create(ApiEndPointInterface.class);
-    apiInterface.createUser(signInLog, signInPassword)
-      .subscribeOn(Schedulers.io())
-      .observeOn(AndroidSchedulers.mainThread())
-      .subscribe(new Observer<Login>() {
-        @Override
-        public void onSubscribe(Disposable d) {
-        }
+                    @Override
+                    public void onComplete() {
+                    }
+                });
+    }
 
-        @Override
-        public void onNext(Login user) {
-          if (signInLog.trim().length() > 0 && signInPassword.trim().length() > 0) {
-            if (signInLog.equals(signInLog) && signInPassword.equals(signInPassword)) {
-              session.createLoginSession(signInLog, signInPassword);
-              Fragment f = RoomFragment.newInstance();
-              getFragmentManager().beginTransaction().replace(R.id.frame_container, f).addToBackStack(null).commit();
-            } else {
-              alert.showAlertDialog(getContext(), "Login failed..", "Username/Password is incorrect", false);
-            }
-          } else {
-            alert.showAlertDialog(getContext(), "Login failed..", "Please enter username and password", false);
-          }
-        }
+  /*  private void createAccount() {
+        //todo : improve logic duplicate
+        ApiEndPointInterface apiInterface = ApiClient.getClient().create(ApiEndPointInterface.class);
+        apiInterface.loginUser(new User(signInLog, signInPassword))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Login>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
 
-        @Override
-        public void onError(Throwable t) {
-          alert.showAlertDialog(getContext(), "java.net.ConnectException:", "Failed to connect to" + BASE_URL, false);
-        }
+                    @Override
+                    public void onNext(Login user) {
+                        if (signInLog.trim().length() > 0 && signInPassword.trim().length() > 0) {
+                            if (signInLog.equals(signInLog) && signInPassword.equals(signInPassword)) {
+                                session.createLoginSession(signInLog, signInPassword);
+                                Fragment f = RoomFragment.newInstance();
+                                getFragmentManager().beginTransaction().replace(R.id.frame_container, f).addToBackStack(null).commit();
+                            } else {
+                                alert.showAlertDialog(getContext(), "Login failed..", "Username/Password is incorrect", false);
+                            }
+                        } else {
+                            alert.showAlertDialog(getContext(), "Login failed..", "Please enter username and password", false);
+                        }
+                    }
 
-        @Override
-        public void onComplete() {
-        }
-      });
-  }
+                    @Override
+                    public void onError(Throwable t) {
+                        alert.showAlertDialog(getContext(), "java.net.ConnectException:", "Failed to connect to" + BASE_URL, false);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                    }
+                });
+    }/*/
 }

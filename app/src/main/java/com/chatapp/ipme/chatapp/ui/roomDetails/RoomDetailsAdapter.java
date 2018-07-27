c07ea54da9f2fb02f101a3860d17644dc77ffe35
Antoine.bret@live.fr <Ragedom1ne>
2018-07-27 15:30:13 +0200

    minor patch

--- app/src/main/java/com/chatapp/ipme/chatapp/ui/roomDetails/RoomDetailsAdapter.java
+++ app/src/main/java/com/chatapp/ipme/chatapp/ui/roomDetails/RoomDetailsAdapter.java
@@ -2,7 +2,6 @@ package com.chatapp.ipme.chatapp.ui.roomDetails;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +13,10 @@ import com.chatapp.ipme.chatapp.session.Chatapp;
 
 import java.util.List;
 
-import static com.chatapp.ipme.chatapp.utils.Constants.VIEW_TYPE_MESSAGE_RECEIVED;
-import static com.chatapp.ipme.chatapp.utils.Constants.VIEW_TYPE_MESSAGE_SENT;
-
 public class RoomDetailsAdapter extends RecyclerView.Adapter {
+
+    private static final int VIEW_TYPE_MESSAGE_SENT = 1;
+    private static final int VIEW_TYPE_MESSAGE_RECEIVED = 2;
 
     private Context context;
     public List<Message> messageList;
